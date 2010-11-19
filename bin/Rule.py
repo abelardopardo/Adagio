0f61a4b63ee4bc9df41a25a50b7682cbd792014b
Abelardo Pardo <abel@tabla>
2010-11-19 08:31:08 +0100

    Creating template to cut/paste rules.

--- bin/Rule.py
+++ bin/Rule.py
@@ -10,33 +10,27 @@ import os, logging, I18n
 import Directory
 
 # Prefix to use for the options
-module_prefix = 'xslt'
+module_prefix = '@PREFIX@'
 
-# List of triplets (varname, default value (if any), description)
+# Set the logger for this module
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(module_prefix)
+
+# List of tuples (varname, default value, description string)
 options = [
     ('exec', 'xsltproc', I18n.get('name_of_executable')),
-    ('debug_level', '${ada.debug_level}', I18n.get('rule_debug_level')),
-    ('src_dir', '${ada.basedir}', I18n.get('rule_src_dir')),
-    ('dst_dir', '${ada.basedir}', I18n.get('rule_dst_dir')),
-    ('style_file', '${ada.home}/ADA_Styles/DocbookProfile.xsl',
-     I18n.get('xslt_style_file')),
+    ('style_file', '%(home)s%(file_separator)sADA_Styles%(file_separator)sDocbookProfile.xsl',
+                   I18n.get('xslt_style_file')),
     ('output_format', 'html', I18n.get('output_format')),
-    ('extra_arguments', None, I18n.get('xslt_extra_arguments')),
-    ('files', None, I18n.get('files_to_process')),
-    ('merge_styles', None, I18n.get('xslt_merge_styles')),
-    ('profile_lang', '${ada.locale}', I18n.get('xslt_profile_lang')),
-    ('multilingual', False, I18n.get('xslt_multilingual'))
+    ('extra_arguments', '', I18n.get('xslt_extra_arguments')),
+    ('merge_styles', '', I18n.get('xslt_merge_styles')),
+    ('languages', '%(locale)s', I18n.get('xslt_languages'))
     ]
 
-def showOptions(directory):
+documentation = """
     """
-    Show the documentation about the different values in the properties
-    """
-    for (a, b, c) in options:
-        print '* ' + a + ' (Value: ' + directory.get(a) + ')'
-        print '  ' + str(c)
 
-def execute():
+def Execute(target, directory):
     """
     Perform the computation with the given symbols
     """
@@ -44,6 +38,4 @@ def execute():
 
 # Execution as script
 if __name__ == "__main__":
-    d = Directory.Directory()
-
-    showOptions(d)
+    pass
