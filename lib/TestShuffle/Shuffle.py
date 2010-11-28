f45dcb542fb26e4d79435543bba89b16266a6a9f
Abelardo Pardo <abel@it.uc3m.es>
2010-11-28 23:00:31 +0100

    Improved the handling of the permutations.

--- lib/TestShuffle/Shuffle.py
+++ lib/TestShuffle/Shuffle.py
@@ -39,16 +39,18 @@ except ImportError:
 
 def main(sourceFile):
     """
-    Function that given a Docbook file containing a set of questions creates as
-    many permutations as specified in a specific element within the
-    document. The document is assumed to have a set of qandaentry elements.
+    Function that given a Docbook file containing a quandaset with a set of
+    quandadivs creates as many permutations as specified in a specific element
+    within the document.
     """
 
+    # For notifying steps through stdout
     stepCount = 1
 
     print 'Step', stepCount, 'Check file permissions'
     stepCount += 1
 
+    # If the given file is not present, return.
     if not os.path.isfile(sourceFile):
         print 'File', sourceFile, 'cannot be accessed.'
         return False
@@ -56,6 +58,7 @@ def main(sourceFile):
     print 'Step', stepCount, 'Create the XML document manager'
     stepCount += 1
     
+    # Parse the source tree.
     try:
         sourceTree = etree.parse(sourceFile)
         sourceTree.xinclude()
@@ -63,14 +66,9 @@ def main(sourceFile):
         print 'Error while parsing', sourceFile
         print e
         return False
-    
-    # Get the root and get/set the id
     root = sourceTree.getroot()
-    rootId = root.get("id")
-    if rootId == None:
-        root.set("id", "AdaShuffle")
 
-    # Get all product numbers from the source document
+    # Get all product numbers from the source document (they are the seeds)
     seedList = []
     sectionInfo = root.find("sectioninfo")
     if sectionInfo != None:
@@ -104,38 +102,41 @@ def main(sourceFile):
 
     # Create a dictionary with all the qandaentries hashed by the index
     # (starting at 1
-    qandadivIndex = []
+    originalOrder = []
     for qandadiv in qandadivs:
         # Get all the entries
         qandaentries = qandadiv.findall('qandaentry')
         # If there is more than one, means several questions inside the same div
         if len(qandaentries) > 1:
-            qandadivIndex.extend(qandaentries)
+            originalOrder.extend(qandaentries)
         # Only one question in the qandadiv
         else:
-            qandadivIndex.append(qandadiv)
+            originalOrder.append(qandadiv)
     
     # Dump all the IDs being processed
     print 'IDs: ',
-    for el in qandadivIndex:
+    for el in originalOrder:
         if el.tag == 'qandadiv':
             print el.get('id'),
         else:
             p = el.getparent()
             print p.get('id') + '_' + str(p.findall('qandaentry').index(el)),
+    print
 
     print 'Step', stepCount, 'Create the permutation vectors'
 
+    # Loop over the elements in the seedList
     permutations = []
     index = 1
     for seedElement in seedList:
         permutation = []
         result = copy.deepcopy(root)
-
+        
         seed = long(seedElement.text)
         random.seed(seed)
 
         # Set the status and replace section info
+        result.set("id", "AdaShuffle")
         result.set('status', str(seed))
         sectionInfo = etree.Element('sectioninfo')
         sectionInfo.extend(seedElement)
@@ -146,6 +147,12 @@ def main(sourceFile):
         qandadivs = qandaset.findall('qandadiv')
         size = len(list(qandadivs))
         
+        # Create a list with all the qandaentries in the given order
+        originalOrder = []
+        for qandadiv in qandadivs:
+            # Get all the entries
+            originalOrder.extend(qandadiv.findall('qandaentry'))
+    
         # Remove the qandadivs (this is to replace them by a shuffled version)
         map(lambda x: qandaset.remove(x), qandadivs)
 
@@ -155,10 +162,12 @@ def main(sourceFile):
         # Reattach the qandadivs
         qandaset.extend(qandadivs)
 
-        # Traverse the qandadivs and shuffle those with more than one qandaentry
+        # Traverse the qandadivs and shuffle those with more than one
+        # qandaentry. We need to preserve their positions within the quandadiv.
         for qandadiv in qandadivs:
             if len(qandadiv.findall('qandaentry')) == 1:
                 continue
+
             # Shuffle the qandaentries inside the qandadiv
             qandaentries = qandadiv.findall('qandaentry')
 
@@ -169,20 +178,24 @@ def main(sourceFile):
             # Remove the qandaentries from the qandadiv
             map(lambda x: qandadiv.remove(x), qandaentries)
 
-            print 'AAA', qandaentries
             for idx in range(0, len(indeces)):
                 entry = qandaentries.pop(random.randint(0, len(qandaentries) - 1))
-                print 'Insert in', indeces[idx], entry
                 qandadiv.insert(indeces[idx], entry)
                 
         result = etree.ElementTree(result)
         result.write(re.sub('\.xml$', '_' + str(index) + '.xml', sourceFile),
                      encoding = 'UTF-8', xml_declaration = True, 
                      pretty_print = True)
+
+        # Get again the qandadivs to dump the permutation array
+        qandadivs = qandaset.findall('qandadiv')
+        print 'V00:',
+        for qandadiv in qandadivs:
+            for qandaentry in qandadiv.findall('qandaentry'):
+                print originalOrder.index(qandaentry),
+        print
+
         index += 1
-    # End of loop for seedElement in seedList
-    print 'Step', stepCount, 'Dumping all permutations'
-    print
 
 
 def dmpPermutations(permutations, idxA, idxB):
