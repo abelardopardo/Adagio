93f85018e2324894eaa6be68cb39ed9f4e6e3479
Abelardo Pardo <abel@it.uc3m.es>
2010-11-30 07:41:48 +0100

    Implemented the print, need to implement getting the file from the dir.

--- bin/Office2pdf.py
+++ bin/Office2pdf.py
@@ -16,11 +16,16 @@ module_prefix = 'soffice'
 options = [
     ('exec', 'soffice', I18n.get('name_of_executable')),
     ('printer', 'PDF', I18n.get('output_format')),
-    ('extra_arguments', '', I18n.get('extra_arguments').format('Inkscape'))
+    ('extra_arguments', '', I18n.get('extra_arguments').format('OpenOffice')),
+    ('printer_dir', os.path.expanduser('$HOME/PDF')
     ]
 
 documentation = {
     'en' : """
+    This target uses OpenOffice and a virtual printer to transform files to PDF
+    documents in a fixed location. The name of the printer must be the value of
+    "printer" and the directory where the files are deposited must be
+    "printer_dir".
     """}
 
 has_executable = AdaRule.which(next(b for (a, b, c) in options if a == 'exec'))
@@ -57,15 +62,9 @@ def Execute(target, directory, pad = ''):
     # Print msg when beginning to execute target in dir
     print pad + 'BB', target
 
-    # Get formats and check if they are empty
-    formats = directory.getWithDefault(target, 'output_format').split()
-    if formats == []:
-        print I18n.get('no_var_value').format('output_format')
-        print pad + 'EE', target
-        return
-
     # Loop over all source files to process
     executable = directory.getWithDefault(target, 'exec')
+    printerName = directory.getWithDefault(target, 'printer')
     extraArgs = directory.getWithDefault(target, 'extra_arguments')
     dstDir = directory.getWithDefault(target, 'dst_dir')
     for datafile in toProcess:
@@ -76,21 +75,43 @@ def Execute(target, directory, pad = ''):
             print I18n.get('file_not_found').format(datafile)
             sys.exit(1)
 
-        # Loop over formats
-        for format in formats:
-            # Derive the destination file name
-            dstFile = os.path.splitext(os.path.basename(datafile))[0] + \
-                '.' + format
-            dstFile = os.path.abspath(os.path.join(dstDir, dstFile))
+        # Derive the destination file name
+        dstFile = os.path.splitext(os.path.basename(datafile))[0] + \
+            '.pdf'
+        dstFile = os.path.abspath(os.path.join(dstDir, dstFile))
 
-            command = [executable, '--export-' + format + '=' + dstFile]
-            command.extend(extraArgs.split())
-            command.append(datafile)
-            
-            # Perform the execution
-            AdaRule.doExecution(target, directory, command, datafile, dstFile,
-                                stdout = Ada.userLog)
+        command = [executable, '-norestore', '-nofirstartwizar', '-nologo',
+                   '-headless', '-pt', printerName]
+        command.extend(extraArgs.split())
+        command.append(datafile)
+        
+        # Check for dependencies if dstFile is given
+        if dstFile != None:
+            srcDeps = directory.option_files
+            if datafile != None:
+                srcDeps.append(datafile)
 
+            Dependency.update(dstFile, set(srcDeps))
+
+            # If the destination file is up to date, skip the execution
+            if Dependency.isUpToDate(dstFile):
+                print I18n.get('file_uptodate').format(os.path.basename(dstFile))
+                return
+            # Proceed with the execution of xslt
+            print I18n.get('producing').format(os.path.basename(dstFile))
+
+        Ada.logDebug(target, directory, 'Popen: ' + ' '.join(command))
+
+        try:
+            pr = subprocess.Popen(command, stdin = stdin, stdout = Ada.userLog,
+                                  stderr = stderr)
+            pr.wait()
+        except:
+            print I18n.get('severe_exec_error').format(command[0])
+            print I18n.get('exec_line').format(' '.join(command))
+            sys.exit(1)
+
+        # Wait for the file to appear
 
     print pad + 'EE', target
     return
