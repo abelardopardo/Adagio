c4bc541f6809db98320d3ad9f6e86ae1782af018
Abelardo Pardo <abel@it.uc3m.es>
2011-01-22 19:39:49 +0100

    Removed encoding to allow presence of unicode

--- bin/Xsltproc.py
+++ bin/Xsltproc.py
@@ -136,7 +136,7 @@ def createStyleTransform(styleList, srcDir = None):
     if len(styles) == 1:
         styleFile = styles[0]
     else:
-        result = StringIO.StringIO('''<?xml version="1.0" encoding="UTF-8"?>
+        result = StringIO.StringIO('''<?xml version="1.0"?>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
   xmlns="http://www.w3.org/1999/xhtml"
   xmlns:xi="http://www.w3.org/2001/XInclude" version="1.0"
