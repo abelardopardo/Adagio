79e31ea24a100630ad9dd3fdcc41deb6627388eb
Abelardo Pardo <abel@it.uc3m.es>
2011-02-11 17:39:54 +0100

    Lowercase all the modules

--- bin/adado.py
+++ bin/adado.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-#
 #
 # Copyright (C) 2010 Carlos III University of Madrid
-# This file is part of the ADA: Agile Distributed Authoring Toolkit
+# This file is part of the Adagio: Agile Distributed Authoring Toolkit
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -22,8 +22,6 @@
 # Author: Abelardo Pardo (abelardo.pardo@uc3m.es)
 import os, sys, getopt, datetime, locale, ConfigParser, codecs, pydoc
 
-import Ada, I18n, Properties, Directory, TreeCache, Dependency
-
 # Fix the output encoding when redirecting stdout
 if sys.stdout.encoding is None:
     (lang, enc) = locale.getdefaultlocale()
@@ -32,17 +30,17 @@ if sys.stdout.encoding is None:
         # sw will encode Unicode data to the locale-specific character set.
         sys.stdout = sw(sys.stdout)
 
+# Import the Adagio package
+_dirName = os.path.dirname(__file__)
+_dirName = os.path.abspath(os.path.join(dirName, '..'))
+sys.path.insert(0, dirName)
+import adagio
+
 def main():
     """
     The manual page for this method is inside the localization package. Check
     the proper [ĺang].py file.
     """
-
-    # Flush all data structures (to simplify debugging)
-    Dependency.flushData()
-    Directory.flushCreatedDirs()
-    Properties.flushConfigParsers()
-    TreeCache.flushData()
 
     #######################################################################
     #
@@ -57,15 +55,15 @@ def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], "c:d:s:f:hxp", [])
     except getopt.GetoptError, e:
-        pydoc.pager(e.msg + '\n' + I18n.get('adado.help'))
+        pydoc.pager(e.msg + '\n' + I18n.get('adagio.help'))
         sys.exit(2)
 
     # Parse the options
     for optstr, value in opts:
         # Choose a different configuration file
         if optstr == "-c":
-            (a, b) = Ada.config_defaults['property_file']
-            Ada.config_defaults['property_file'] = (value, b)
+            (a, b) = adagio.config_defaults['property_file']
+            adagio.config_defaults['property_file'] = (value, b)
 
         # Debug option
         elif optstr == "-d":
@@ -76,8 +74,8 @@ def main():
                 print I18n.get('incorrect_debug_option')
 	        print str(e)
                 sys.exit(3)
-            (a, b) = Ada.config_defaults['debug_level']
-            Ada.config_defaults['debug_level'] = (value, b)
+            (a, b) = adagio.config_defaults['debug_level']
+            adagio.config_defaults['debug_level'] = (value, b)
 
         # Change directory
         elif optstr == "-f":
@@ -93,18 +91,18 @@ def main():
             if toDir != '':
                 os.chdir(toDir)
             # Set the property_file to cFile
-            (a, b) = Ada.config_defaults['property_file']
-            Ada.config_defaults['property_file'] = (cFile, b)
+            (a, b) = adagio.config_defaults['property_file']
+            adagio.config_defaults['property_file'] = (cFile, b)
 
         # Dump the manual page
         elif optstr == "-h" or optstr == "-x":
-            pydoc.pager(I18n.get('adado.help'))
+            pydoc.pager(I18n.get('adagio.help'))
             sys.exit(3)
 
         # Allow for partial execution (if some tools are missing
         elif optstr == "-p":
-            (a, b) = Ada.config_defaults['partial']
-            Ada.config_defaults['partial'] = (1, b)
+            (a, b) = adagio.config_defaults['partial']
+            adagio.config_defaults['partial'] = (1, b)
 
         # Set a value in the environment
         elif optstr == "-s":
@@ -112,7 +110,7 @@ def main():
             # If incorrect number of arguments, stop processing
             if len(sname_value) != 3:
                 pydoc.pager(I18n.get('incorrect_arg_num').format('-s option')
-                               + '\n' + I18n.get('adado.help'))
+                               + '\n' + I18n.get('adagio.help'))
                 sys.exit(3)
             optionsToSet.append(' '.join(sname_value))
 
@@ -125,16 +123,16 @@ def main():
     # Initialization of all the required variables
     #
     #######################################################################
-    Ada.initialize()
+    adagio.initialize()
 
     # The rest of the arguments are the targets
     targets = args
 
     # Print arguments
-    Ada.logDebug('main', None, 'Argv: ' + ' '.join(sys.argv))
+    adagio.logDebug('main', None, 'Argv: ' + ' '.join(sys.argv))
 
     # Print targets
-    Ada.logDebug('main', None, 'Targets: ' + ' '.join(targets))
+    adagio.logDebug('main', None, 'Targets: ' + ' '.join(targets))
 
     #######################################################################
     #
@@ -157,7 +155,7 @@ def main():
     # Termination
     #
     #######################################################################
-    Ada.finish()
+    adagio.finish()
 
 # Execution as script
 if __name__ == "__main__":
