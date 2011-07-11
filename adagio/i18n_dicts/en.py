34c0894f94533123168c995a670331d126f2ca6f
Abelardo Pardo <abel@it.uc3m.es>
2011-07-11 09:44:08 +0200

    Removed unnecesary messages.

--- adagio/i18n_dicts/en.py
+++ adagio/i18n_dicts/en.py
@@ -140,37 +140,31 @@ msgs = {
     'default_file_separator': 'Separator used by the system in file paths',
     'default_files': 'Source files to use in a rule',
     'default_help': 'Documentation about what the rule does',
-    'default_home': 'Directory where ADA is installed',
+    'default_home': 'Directory where Adagio is installed',
     'default_languages': """Space separated list of languages to consider when
     processing the files (when appropriate)""",
-    'default_partial': """0/1 value stating if ADA continues when a rule cannot be 
+    'default_partial': """0/1 value stating if Adagio continues when a rule cannot be 
     executed""",
     'default_project_file': 'File with project-wide declarations',
     'default_project_home': 'Directory where the project file is found',
     'default_property_file': 'File name with the rule definitions',
     'default_src_dir': 'Directory from where to take the source files',
-    'default_version': 'Current ADA version',
+    'default_version': 'Current Adagio version',
     ## End of config_defaults
-    'checking_configuration': 'Checking ADA configuration',
     'no_help_available': 'There is no documentation for this rule.',
-    'help_option': 'Text explaining the tasks carried by this rule.',
-    'fatal_error': 'Fatal error encountered. Attach "adagio.log" to notification.',
     'file_not_found': 'File {0} not found',
-    'dir_not_found': 'Directory {0} not found',
-    'dir_created': 'Directory {0} created',
     'not_a_directory': '{0} is not a directory',
     'producing': 'Producing {0}',
     'copying': 'Copying {0}',
     'processing': 'Processing {0}',
     'removing': 'Removing {0}',
+    'user_defined_variable': 'User defined variable',
     'xslt_empty_result': 'XSLT transformation result is empty',
     'no_file_to_process' : 'No file given to process',
-    'no_dir_to_process' : 'No directory given to process',
     'no_style_file' : 'No style file given.',
-    'no_doc_for_rule': 'No documentation for rule {0}',
     'no_executable': 'Application {0} not present in the system.',
-    'doc_preamble' : '===== {0} Processing Rules =====',
-    'var_preamble' : '===== {0} Variables        =====',
+    'doc_preamble' : '=============== {0} Processing Rules ===============',
+    'var_preamble' : '=============== {0} Variables        ===============',
     'cannot_open_file': 'Cannot open file {0}',
     'circular_include': 'Circular chain of templates detected:',
     'circular_alias': 'Circular chain of aliases detected:',
@@ -179,16 +173,15 @@ msgs = {
     'files': 'Files',
     'template_error': 'Incorrect template in file {0}.\n' + 
     'Rule must define only the variable "files"',
-    'incorrect_assignment': 'Incorrect assignment in line {ln} of {pfile}',
     'incorrect_version_format': 
     'Incorrect version {0}. Should have major.minor.patch structure',
-    'incorrect_version' : 'Incorrect ADA Version ({0}). Review variables ' + \
-        'ada.exact_version, ada.minimum_version and ' + \
-        'ada.maximum_version',
+    'incorrect_version' : 'Incorrect Adagio Version ({0}). Review variables ' + \
+        'adagio.exact_version, adagio.minimum_version and ' + \
+        'adagio.maximum_version',
     'incorrect_variable_reference': 'Incorrect reference in value {0}',
     'import_error': 'Error while importing script in {0}',
     'import_collision': 
-    'Script {0} collides with another ADA script. Name change required',
+    'Script {0} collides with another Adagio script. Name change required',
     'build_function_name': 'Function to call in the script.',
     'clean_function_name': 'Function to call in the script in rule "clean"',
     'function_error': 'Error when executing function {0}',
@@ -205,87 +198,57 @@ Check if docbook-xsl and docbook-xml are properly installed.""",
 The format must be 'name1': 'value2', 'name2': 'value2'...""",
     'unknown_rule': 'Unknown rule {0}.',
     'file_uptodate': '{0} up to date. Bypassing.',
-    'not_enough_params': 'Not enough params for {0}',
     'no_var_value': 'No value given in variable {0}',
     'incorrect_arg_num': 'Incorrect arguments for {0}',
     'incorrect_option': 'Incorrect option {0}',
     'incorrect_option_in_file': 'Incorrect option {0} in file {1}',
-    'incorrect_option_assignment': 'Incorrect assignment {0} = {1}',
-    'cannot_detect_ada_home' : 'Unable to set variable ada.home',
+    'cannot_detect_adagio_home' : 'Unable to set variable adagio.home',
     'cannot_find_properties' : 'No file {0} in directory {1}',
     'name_of_executable': 'Name of the executable to use in this rule',
-    'rule_debug_level': 'Level of debug message',
-    'rule_src_dir': 'Directory containing the source files.',
     'rsync_src_dir': 'Source directory to copy from',
     'rsync_dst_dir': 'Destination directory to synchronize',
     'rule_dst_dir': 'Directory where the new files will be created.',
     'convert_geometry': 'Geometry used to convert the files',
-    'convert_output_suffix':
-    'Suffix to add to files when processed with "convert"',
     'convert_crop_option': 'Crop option -crop widthxheight+x+y to convert',
     'dblatex_compliant':
-    'If non zero Ada-flavored behavior (suppress version page)',
+    'If non zero Adagio-flavored behavior (suppress version page)',
     'xslt_style_file': 'Style to be applied to the given source files.',
     'script_input_file': 'File to use as script input',
     'script_output_file': 'File to write the srcipt output',
     'script_error_file': 'File to write the script errors',
+    'script_arguments': 'Arguments passed to the given Python script',
     'output_format': 'Extension to use when creating the new files.',
     'empty_output_format': 'Option output_format in rule {0} cannot be empty.',
     'gimp_script': 'Script to process gimp files in batch mode',
     'extra_arguments':
     """Extra arguments passed directly to {0}. 
      Format: comma separated list of pairs name: value""",
-    'files_to_process': 'Space separated list of files to process.',
-    'xslt_merge_styles':
-    'Space separated list of styles to combine with the given style',
-    'languages': 'Space separated list of languages to consider',
-    'date_format': 'Format used to manipulate the dates',
     'date_incorrect_format': 'Date {0} not compliant with format {1}',
     'program_incorrect_format': '{0} cannot process format {1}',
     'exercise_produce': 
     'Exercise versions produced. Any subset of {regular, solution, pguide, submit}',
     'export_dst': 'Destination of the exports',
     'export_rules': 'Rules to execute in the destination directories',
-    'export_begin': 'Date/time beyond which the export is allowed.',
-    'export_end': 'Date/time until the export is allowed.',
-    'export_open': 'If the export is enabled (must be "0" or "1").',
     'export_no_dst': 'No destination for export. Bypassing.',
     'enable_not_open': 'Rule not executed because "open" is {0}',
     'enable_not_revision': 
     """Rule not executed. Value of "{0}.enable_profile" is not in
-"ada.enabled_profiles".""",
-    'files_included_from': 
-    'List of files to parse and obtain the included files and visit their directories', 
+"adagio.enabled_profiles".""",
     'enable_closed_begin': 'Rule not executed because it is before {0}',
     'enable_closed_end': 'Rule not executed because it is after {0}',
-    'testexam_permutations': 'Number of versions with the questions shuffled',
     'testexam_no_shuffle_required': 'No shuffle required for file {0}',
     'testexam_shuffling': 'Shuffling questions in file {0}',
-    'testexam_error_shuffling': 'Error while shuffling questions in test exam',
-    'ada_current_datetime': 'The Current date/time to be considered',
-    'ada_enabled_profiles':
+    'adagio_enabled_profiles':
     'Space separated list of values to use to enable rule execution',
-    'ada_minimum_version':
-    'The minimum ada version that is required to execute this directory',
-    'ada_maximum_version':
-    'The maximum ada version that is required to execute this directory',
-    'ada_exact_version':
-    'The exact ada version that is required to execute this directory',
-    'ada_debug_level_option':
-    'Level of messages to print: 0 = None, 1, 2, 3, 4, 5 = CRITICAL',
-    'circular_directory':
-    'Circular dependency to directory {0}.',
+    'adagio_minimum_version':
+    'The minimum Adagio version that is required to execute this directory',
+    'adagio_maximum_version':
+    'The maximum Adagio version that is required to execute this directory',
+    'adagio_exact_version':
+    'The exact Adagio version that is required to execute this directory',
     'circular_execute_directory':
     'Circular execution dependency to directory {0}.',
     'illegal_rule_name': 'Rule {t} is not known in dir {dl}.',
     'incorrect_debug_option' : 'Debug option requires an integer as parameter',
-    'ada_version_option' : 'Current ADA version',
-    'ada_locale_option' : 'Locale used while executing ADA',
-    'ada_home_option' : 'Directory where ADA is installed',
-    'ada_property_file_option' : 'File containing the definitions',
-    'ada_file_separator_option' : 'Character to separate filenames',
-    'pdfnup_nup': 'How to divide the pages, default 2 per page.',
-    'option': 'Option',
-    'options': 'Options',
-    'not_found': 'not found'
+    'pdfnup_nup': 'How to divide the pages, default 2 per page.'
     }
