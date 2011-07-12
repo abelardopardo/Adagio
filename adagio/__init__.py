03eeeff3c56f85db8248077c148172bce9108ff6
Abelardo Pardo <abel@it.uc3m.es>
2011-07-12 20:00:57 +0200

    Version rename

--- adagio/__init__.py
+++ adagio/__init__.py
@@ -73,7 +73,7 @@ config_defaults = {
     'project_home':       (_currentDir, i18n.get('default_project_home')),
     'property_file':      ('Properties.dgo', i18n.get('default_property_file')),
     'src_dir':            (_currentDir, i18n.get('default_src_dir')),
-    'version':            ('11.07.2', i18n.get('default_version'))
+    'version':            ('11.07.3', i18n.get('default_version'))
 }
 
 # List of tuples (varname, default value, description string)
